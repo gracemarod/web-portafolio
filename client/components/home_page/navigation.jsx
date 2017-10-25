2813e8ea54e20d76b6e8f8c85a0de4e9aa3f871a
Grace Rodriguez <gracemarod@gmail.com>
2017-10-25 12:47:54 -0400

    Active tab on navbar works! Yey

--- client/components/home_page/navigation.jsx
+++ client/components/home_page/navigation.jsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
 import { Nav, NavItem} from 'react-bootstrap';
 
 
-function handleSelect(selectedKey) {
-  alert('selected ' + selectedKey);
-}
-
 class Navbar extends Component {
-
+constructor(props) {
+  super(props);
+  this.state = {
+    key: 1
+  };
+ this.handleSelect = this.handleSelect.bind(this)
+}
+handleSelect(key) {
+  this.setState({key});
+}
 
 	render() {
 		return(
-		  <Nav className="nav" >
-		  	<ul>
-			    <NavItem className="navitem" eventKey={1}><li><a>Home</a></li></NavItem>
-			    <NavItem className="navitem" eventKey={2}><li><a>Education</a></li></NavItem>
-			    <NavItem className="navitem" eventKey={3}><li><a>Skills</a></li></NavItem>
-			    <NavItem className="navitem" eventKey={4}><li><a>Projects</a></li></NavItem>
-			    <NavItem className="navitem" eventKey={5}><li><a>Blog</a></li></NavItem>
-		 	</ul>
+		  <Nav className="nav" activeKey={this.state.key} onSelect={this.handleSelect} id="controlled-tab-example">
+			    <NavItem eventKey={1} title="Tab 1"><li><a>Home</a></li></NavItem>
+			    <NavItem eventKey={2}><li><a>Education</a></li></NavItem>
+			    <NavItem eventKey={3}><li><a>Skills</a></li></NavItem>
+			    <NavItem eventKey={4}><li><a>Projects</a></li></NavItem>
+			    <NavItem eventKey={5}><li><a>Blog</a></li></NavItem>
+		 
 		  </Nav>
 	 )}
 }
